0ded8465775b2aa1ac94a8411ff26e0d3c7f489c
Midas van Oene <midas.van.oene@gmail.com>
2017-11-04 03:16:06 +0100

    ehh

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-function ytlt() {
+function ytlt(baseUrl) {
 	var self = this;
+	var _baseUrl = baseUrl;
 	var __construct = function() {
 		self._settings = {
 		}
+
+		self.tryInitialValue = function() {
+			var fullpath = $(location).attr('href');
+			var path = self.getPath(fullpath);
+			if(path.length == 0) {
+				self.setValue("You talk like this");
+			} else {
+				var queryInput = "";
+				queryInput = path.replace("+", " ");
+				queryInput = queryInput.replace("?", "");
+				self.setValue(queryInput);
+			}
+		}
+
+		self.getPath = function(queryString) {
+			return queryString.replace(_baseUrl, "")
+		}
+
 		self.trackInput = function() {
 			var e = document.getElementById("wrapper");
 			e.addEventListener("input", function() {
@@ -13,16 +32,25 @@ function ytlt() {
 		self.performReplace = function() {
 			var currentValue = self.getValue().toLowerCase();
 			console.log("Changing '" + currentValue + "'...");
-			var maxLowerCaseAmount = 2;
+			var maxLowerCaseAmount = 1;
 			var currentLowerCaseAmount = 0;
 			var newValue = "";
 			for(var i = 0; i < currentValue.length; i ++) {
-				if(this.randomChoice()) {
-					newValue += currentValue[i].toUpperCase();
-				} else {
+				if(maxLowerCaseAmount < currentLowerCaseAmount) {
+					currentLowerCaseAmount = 0;
+					// simple add the lowercased value
 					newValue += currentValue[i];
+				} else {
+					if(this.randomChoice()) {
+						newValue += currentValue[i].toUpperCase();
+					} else {
+						newValue += currentValue[i].toLowerCase();
+						currentLowerCaseAmount++;
+					}
 				}
 			}
+			console.log(currentLowerCaseAmount);
+
 			self.setValue(newValue);
 		};
 
